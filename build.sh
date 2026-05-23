e8125126d7307ee82d20f267696e46790a02b26b
nianob <nianobMc@gmail.com>
2026-05-23 13:27:16 +0200

    updated build files

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pyinstaller -y --onefile -n "Neuro 21 Station Player" -i "./data/icon.ico" --add-binary ffmpeg/linux:ffmpeg/linux --add-data data:data --windowed ./player.py
+pyinstaller -y --onefile -n "Neuro 21 Station Player" -i "./data/icon.ico" --add-binary ffmpeg/linux:ffmpeg/linux --add-data data:data --windowed ./player_v2.py
